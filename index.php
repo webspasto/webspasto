774108bc457ea41d7688f6f6dd019d074ef39b7b
SAP <SAP@10.0.0.21>
2014-12-13 09:12:14 -0500

    Andres S

--- index.php
+++ index.php
@@ -4,14 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Webs Pasto</title>
 </head>
-<style>
-	*{
-		background: black;
-		color: white;
-		font-size: 30px;
-		text-align: center;
-	}
-</style>
+
 <body>
 	<h1>Webs Pasto</h1>
 </body>
