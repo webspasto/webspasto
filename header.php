dfafeba09fee677ad66da3b531532d9bb2fa064a
Juan Pablo <juan.rr.08@gmail.com>
2014-12-15 08:38:11 -0500

    By Juan

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <a href="https://twitter.com/webspasto" title="Twitter"><div id="tw"></div></a>
             <a href="https://www.facebook.com/pages/Desarrollo-Web-Pasto/382046511962765" title="Facebook"><div id="fb"></div></a>
             <a href="https://plus.google.com/u/0/105315169514370720149/" title="Google +"><div id="go"></div></a>
-            <!--<a href="https://www.youtube.com" title="Youtube"><div id="yt"></div></a>-->
+            <a href="https://www.youtube.com/channel/UCsKneHB6ja_sI2qa6tHmUyg/feed" title="Youtube"><div id="yt"></div></a>
         </div>
         <div id="logo_cabecera"></div>
     </div>
