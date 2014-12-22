8f083d3ec615771b6974578ecb5ca0073764e220
SAP <SAP@10.0.0.21>
2014-12-22 08:33:05 -0500

    Andres S

--- js/javascript.js
+++ js/javascript.js
@@ -20,34 +20,65 @@ function mostrar(idElemento,cantidadIN ,alturaIN ,anchuraIN ,esperaIN ,velocidad
 	}
 	else{
 		inciarValoresMostrar(cantidadIN ,alturaIN ,anchuraIN ,esperaIN ,velocidadIN);
-		document.getElementById(idElemento).style.overflow="hidden";
+		document.getElementById(idElemento).style.overflow="scroll";
 		animar(idElemento);
 	}
 }
 
-function ocultar(idElemento){
+function ocultar(idElemento,velocidadIN){
 	elemento = document.getElementById(idElemento);
-	elemento.style.overflow="hidden";
+	elemento.style.overflow="scroll";
 	alturaIN = elemento.style.pixelHeig;
     alturaIN = elemento.offsetHeight;
-	inciarValoresMostrar(alturaIN ,alturaIN ,elemento.offsetWidth ,1 ,0.5 );
+	inciarValoresMostrar(alturaIN ,alturaIN ,elemento.offsetWidth ,1 ,velocidadIN);
 	animar(idElemento);
 }
 
 function animar(idElemento){
 	if(cantidad==1 || cantidad==altura){
 		incremento=(cantidad==1)?velocidad:-velocidad;
-		tope=(cantidad==1)?altura:1;
+		tope=(cantidad==1)?altura:0;
 	}	
 	elemento=document.getElementById(idElemento);
 	cantidad+=incremento;
 	elemento.style.height=cantidad+"px";
 	elemento.style.width=parseInt(prorrata*cantidad)+"px";
-	if(cantidad==tope-1){
-		cantidad++;
-		cantidad--;
+	if((cantidad==1 || cantidad < 0) && tope ==0){
+		cantidad=tope;
+		elemento.style.height=0+"px";
+		elemento.style.width=0+"px";
 	}
 	if(cantidad!=tope){
 		setTimeout("animar('"+idElemento+"')",espera);
 	}
+}
+
+function acordeon(idElementoAnimar,idDivPadre)
+{
+    abrir = false;
+    if(!estaAbierto(idElementoAnimar))
+        abrir=true;
+    padre = document.getElementById(idDivPadre);  
+        array = padre.getElementsByTagName('div');
+        for(i = 0; i<array.length ;i++){
+            div = array[i];
+            id=div.getAttribute('id')
+            if(estaAbierto(id) && id !=idElementoAnimar)
+                setTimeout("ocultar('"+id+"',1)",1); 
+    }
+
+    if(abrir)
+        setTimeout("mostrar('"+idElementoAnimar+"',1,100,320,1,3)",500); 
+    else
+        setTimeout("ocultar('"+idElementoAnimar+"',1)",1); 
+}
+function estaAbierto(idElemento){
+    elemento = document.getElementById(idElemento);  
+    alturaIN = elemento.style.pixelHeig;
+    alturaIN = elemento.offsetHeight;
+    anchura  = elemento.offsetWidth;
+
+    if(alturaIN == 0 || anchura == 0)
+        return false;
+    return true;
 }
