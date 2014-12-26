484b5b81d5a63f5d21f8813ac18d5cba28944bec
Juan Pablo <juan.rr.08@gmail.com>
2014-12-26 15:54:41 -0500

    By Juan

--- enviarEmail.php
+++ enviarEmail.php
@@ -5,50 +5,34 @@
 </head>
 <body>
     <?php
-    include("lib/class.phpmailer.php");
-    include("lib/class.smtp.php");
-    $mail = new PHPMailer();
-    $mail->IsSMTP();
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = "ssl";
-    //$mail->SMTPDebug = 1;
-    $mail->Host = "ssl://smtp.gmail.com";
-    $mail->Port = 465;
-    $mail->Username = "webspasto@gmail.com";
-    $mail->Password = "Casitegano";
-   //$mail->Priority = 1;
-
-    /*$mail->IsSendmail();
-    $mail->Mailer = "smtp";
-    $mail->SMTPDebug = 1;
-    $mail->Host = 'mx1.hostinger.es';
-    $mail->Port = 2525;
-    $mail->SMTPAuth = true;
-    $mail->Username = 'webspasto@gmail.com';
-    $mail->Password = 'Casitegano';
-    $mail->SMTPSecure = '';*/
-
     $nombre = $_GET['nombre'];
     $email = $_GET['email'];
     $asunto = $_GET['asunto'];
     $consulta = $_GET['mensaje'];
-    $mail->From = $email;
-    $mail->FromName = $nombre;
-    $mail->Subject = $asunto;
-    $mail->AltBody = "Mensaje de WP";
 
-    $mensaje = "<div style='padding:5px; background:#C6C6C6; border:0px solid black;''><div><p><strong>Nombre:</strong> " . $nombre . "</p></div><div><p><strong>Email: </strong>" . $email . "</p></div><div style='text-align:justify;'><p><strong>Consulta: </strong>" . $consulta . "</p></div></div>";
-    $mail->MsgHTML($mensaje);
-    $mail->AddAddress("webspasto@gmail.com", "");
-    $mail->IsHTML(true);
+    $mensaje = "<div style='padding:5px; background:#EEE; border:0px solid black; text-align:justify;'>"
+            . "<div><p><strong>Nombre:</strong> " . $nombre . "</p></div>"
+            . "<div><p><strong>Email: </strong>" . $email . "</p></div>"
+            . "<div><p><strong>Mensaje: </strong>" . $consulta . "</p></div>"
+            . "</div>";
+    
+    $para = "webspasto@webspasto.hol.es";
+    $headers = "From: " . $nombre . "<" . $email . ">\r\n";
+    $headers .= "Reply-To: " . $nombre . "<" . $email . ">\r\n";
+    $headers .= "CC: " . $para . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $mail->SetFrom($email, $nombre);
-    $mail->AddReplyTo($email, $nombre);
-
-    if (!$mail->Send()) {
-        echo "<span style='color: red;'>Error, su mensaje no pudo ser enviado.</span><br><a id='amsg' onclick='javascript:limpiarContacto();'><img id='imgmsg' title='Limpiar' src='img/img_error.png'></a>";
+    if (!mail($para, $asunto, $mensaje, $headers)) {
+        echo "<span style='color: red;'>"
+                . "Error, su mensaje no pudo ser enviado.</span><br>"
+                . "<a id='amsg' onclick='javascript:limpiarContacto();'>"
+                . "<img id='imgmsg' title='Limpiar' src='img/img_error.png'></a>";
     } else {
-        echo "<span style='color: green;'>Gracias, su mensaje ha sido enviado.</span><br><a id='amsg' title='Limpiar' onclick='javascript:limpiarContacto();'><img id='imgmsg' src='img/img_exito.png'></a>";
+        echo "<span style='color: green;'>"
+                . "Gracias, su mensaje ha sido enviado.</span><br>"
+                . "<a id='amsg' title='Limpiar' onclick='javascript:limpiarContacto();'>"
+                . "<img id='imgmsg' src='img/img_exito.png'></a>";
     }
 ?>
 </body>
