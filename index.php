3719432092b02b5e81e1a5a0a0ded2eb130543a9
Juan Pablo <juan.rr.08@gmail.com>
2014-12-27 11:35:06 -0500

    By Juan

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="description" content="Desarrollo de aplicaciones web y de escritorio">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title>WebsPasto - Aplicaciones Web y Escritorio</title>
         <link rel="stylesheet" type="text/css" href="css/estilo.css">
         <link rel="shortcun icon" href="img/icon.ico" type="image/png">
